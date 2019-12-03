96876aebd34509acfc20028bb317a4f434c2bed6
Jay <niujay@gmail.com>
2019-12-03 16:03:58 +0800

    type corrected for self.conv1

--- experts_quickstart.py
+++ experts_quickstart.py
@@ -25,7 +25,7 @@ test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 class MyModel(Model):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.conv1 = Conv2D(32, 32, activation='relu')
+        self.conv1 = Conv2D(32, 3, activation='relu')
         self.flatten = Flatten()
         self.d1 = Dense(128, activation='relu')
         self.d2 = Dense(10, activation='softmax')
