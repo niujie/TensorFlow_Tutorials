b61e1c6a1d9fc25c5c958fd0a6cbf7c8ac8d4acd
Jay <niujay@gmail.com>
2019-12-04 21:40:38 +0800

    contents added

--- ML_basics_with_Keras/text_classification_preprocessed.py
+++ ML_basics_with_Keras/text_classification_preprocessed.py
@@ -25,3 +25,106 @@ print(tf.__version__)
     as_supervised=True,
     # Also return the `info` structure.
     with_info=True)
+
+# Try the encoder
+# The dataset info includes the text encoder
+# (a tfds.features.text.SubwordTextEncoder)
+encoder = info.features['text'].encoder
+
+print('Vocabulary size: {}'.format(encoder.vocab_size))
+
+sample_string = 'Hello TensorFlow.'
+
+encoded_string = encoder.encode(sample_string)
+print('Encoded string is {}'.format(encoded_string))
+
+original_string = encoder.decode(encoded_string)
+print('The original string: "{}"'.format(original_string))
+
+assert original_string == sample_string
+
+for ts in encoded_string:
+    print('{} ----> {}'.format(ts, encoder.decode([ts])))
+
+# Explore the data
+for train_example, train_label in train_data.take(1):
+    print('Encoded text:', train_example[:10].numpy())
+    print('Label:', train_label.numpy())
+
+print(encoder.decode(train_example))
+
+# Prepare the data for training
+BUFFER_SIZE = 1000
+
+train_batches = (
+    train_data
+    .shuffle(BUFFER_SIZE)
+    .padded_batch(32, train_data.output_shapes))
+
+test_batches = (
+    test_data
+    .padded_batch(32, train_data.output_shapes))
+
+for example_batch, label_batch in train_batches.take(2):
+    print("Batch shape:", example_batch.shape)
+    print("label shape:", label_batch.shape)
+
+# Build the model
+model = keras.Sequential([
+  keras.layers.Embedding(encoder.vocab_size, 16),
+  keras.layers.GlobalAveragePooling1D(),
+  keras.layers.Dense(1, activation='sigmoid')])
+
+model.summary()
+
+# Loss function and optimizer
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+history = model.fit(train_batches,
+                    epochs=10,
+                    validation_data=test_batches,
+                    validation_steps=30)
+
+# Evaluate the model
+loss, accuracy = model.evaluate(test_batches)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+# Create a graph of accuracy and loss over time
+history_dict = history.history
+print(history_dict.keys())
+
+import matplotlib.pyplot as plt
+
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+plt.clf()   # clear figure
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+
+plt.show()
