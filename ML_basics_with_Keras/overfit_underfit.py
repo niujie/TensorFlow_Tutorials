843f33f492047aa40ed0359b38bffb9e0bb90d20
Jay <niujay@gmail.com>
2019-12-05 12:25:57 +0800

    contents added

---
+++ ML_basics_with_Keras/overfit_underfit.py
@@ -0,0 +1,260 @@
+# Overfit and underfit
+
+# Setup
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+
+print(tf.__version__)
+
+import tensorflow_docs as tfdocs
+import tensorflow_docs.modeling
+import tensorflow_docs.plots
+
+from IPython import display
+from matplotlib import pyplot as plt
+
+import numpy as np
+
+import pathlib
+import shutil
+import tempfile
+
+logdir = pathlib.Path(tempfile.mkdtemp()) / "tensorboard_logs"
+shutil.rmtree(logdir, ignore_errors=True)
+
+# The Higgs Dataset
+gz = tf.keras.utils.get_file('HIGGS.csv.gz',
+                             'https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz')
+
+FEATURES = 28
+ds = tf.data.experimental.CsvDataset(gz, [float(), ] * (FEATURES + 1), compression_type="GZIP")
+
+
+def pack_row(*row):
+    _label = row[0]
+    _features = tf.stack(row[1:], 1)
+    return _features, _label
+
+
+packed_ds = ds.batch(10000).map(pack_row).unbatch()
+
+for features, label in packed_ds.batch(1000).take(1):
+    print(features[0])
+    plt.hist(features.numpy().flatten(), bins=101)
+    plt.show()
+
+N_VALIDATION = int(1e3)
+N_TRAIN = int(1e4)
+BUFFER_SIZE = int(1e4)
+BATCH_SIZE = 500
+STEPS_PER_EPOCH = N_TRAIN // BATCH_SIZE
+
+validate_ds = packed_ds.take(N_VALIDATION).cache()
+train_ds = packed_ds.skip(N_VALIDATION).take(N_TRAIN).cache()
+print(train_ds)
+
+validate_ds = validate_ds.batch(BATCH_SIZE)
+train_ds = train_ds.shuffle(BUFFER_SIZE).repeat().batch(BATCH_SIZE)
+
+# Demonstrate overfitting
+# Training procedure
+lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+    0.001,
+    decay_steps=STEPS_PER_EPOCH * 1000,
+    decay_rate=1,
+    staircase=False)
+
+
+def get_optimizer():
+    return tf.keras.optimizers.Adam(lr_schedule)
+
+
+# The code above sets a schedules.InverseTimeDecay to
+# hyperbolically decrease the learning rate to 1/2 of the base rate
+# at 1000 epochs, 1/3 at 2000 epochs and so on.
+
+step = np.linspace(0, 100000)
+lr = lr_schedule(step)
+plt.figure(figsize=(8, 6))
+plt.plot(step / STEPS_PER_EPOCH, lr)
+plt.ylim([0, max(plt.ylim())])
+plt.xlabel('Epoch')
+_ = plt.ylabel('Learning Rate')
+plt.show()
+
+
+def get_callbacks(name):
+    return [
+        tfdocs.modeling.EpochDots(),
+        tf.keras.callbacks.EarlyStopping(monitor='val_binary_crossentropy', patience=200),
+        tf.keras.callbacks.TensorBoard(logdir / name),
+    ]
+
+
+def compile_and_fit(model, name, optimizer=None, max_epochs=10000):
+    if optimizer is None:
+        optimizer = get_optimizer()
+    model.compile(optimizer=optimizer,
+                  loss='binary_crossentropy',
+                  metrics=['accuracy', 'binary_crossentropy'])
+
+    model.summary()
+
+    history = model.fit(
+        train_ds,
+        steps_per_epoch=STEPS_PER_EPOCH,
+        epochs=max_epochs,
+        validation_data=validate_ds,
+        callbacks=get_callbacks(name),
+        verbose=0)
+    return history
+
+
+# Tiny model
+# Start by training a linear model
+tiny_model = tf.keras.Sequential([
+    layers.Dense(16, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(1, activation='sigmoid')
+])
+
+size_histories = {'Tiny': compile_and_fit(tiny_model, 'sizes/Tiny')}
+
+plotter = tfdocs.plots.HistoryPlotter(metric='binary_crossentropy', smoothing_std=10)
+plotter.plot(size_histories)
+plt.ylim([0.5, 0.7])
+plt.show()
+
+# Small model
+small_model = tf.keras.Sequential([
+    # `input_shape` is only required here so that `.summary` works.
+    layers.Dense(16, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(16, activation='elu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+size_histories['Small'] = compile_and_fit(small_model, 'sizes/Small')
+
+# Medium model
+medium_model = tf.keras.Sequential([
+    layers.Dense(64, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+size_histories['Medium'] = compile_and_fit(medium_model, "sizes/Medium")
+
+# Large model
+large_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+size_histories['large'] = compile_and_fit(large_model, "sizes/large")
+
+# Plot the training and validation losses
+plotter.plot(size_histories)
+a = plt.xscale('log')
+plt.xlim([5, max(plt.xlim())])
+plt.ylim([0.5, 0.7])
+plt.xlabel("Epochs [Log Scale]")
+plt.show()
+
+# View in TensorBoard
+# %tensorboard --logdir {logdir}/sizes
+
+display.IFrame(
+    src="https://tensorboard.dev/experiment/vW7jmmF9TmKmy3rbheMQpw/#scalars&_smoothingWeight=0.97",
+    width="100%", height="800px")
+plt.show()
+
+# !tensorboard dev upload --logdir  {logdir}/sizes
+
+# Strategies to prevent overfitting
+shutil.rmtree(logdir / 'regularizers/Tiny', ignore_errors=True)
+shutil.copytree(logdir / 'sizes/Tiny', logdir / 'regularizers/Tiny')
+
+regularizer_histories = {'Tiny': size_histories['Tiny']}
+
+# Add weight regularization
+l2_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001),
+                 input_shape=(FEATURES,)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(1, activation='sigmoid')
+])
+
+regularizer_histories['l2'] = compile_and_fit(l2_model, "regularizers/l2")
+
+plotter.plot(regularizer_histories)
+plt.ylim([0.5, 0.7])
+plt.show()
+
+# More Info
+result = l2_model(features)
+regularization_loss = tf.add_n(l2_model.losses)
+
+dropout_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu', input_shape=(FEATURES,)),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(1, activation='sigmoid')
+])
+
+regularizer_histories['dropout'] = compile_and_fit(dropout_model, "regularizers/dropout")
+
+plotter.plot(regularizer_histories)
+plt.ylim([0.5, 0.7])
+plt.show()
+
+# Combined L2 + dropout
+combined_model = tf.keras.Sequential([
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu', input_shape=(FEATURES,)),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(1, activation='sigmoid')
+])
+
+regularizer_histories['combined'] = compile_and_fit(combined_model, "regularizers/combined")
+
+plotter.plot(regularizer_histories)
+plt.ylim([0.5, 0.7])
+plt.show()
+
+# View in TensorBoard
+# %tensorboard --logdir {logdir}/regularizers
+
+display.IFrame(
+    src="https://tensorboard.dev/experiment/fGInKDo8TXes1z7HQku9mw/#scalars&_smoothingWeight=0.97",
+    width="100%",
+    height="800px")
+plt.show()
+
+# !tensorboard dev upload --logdir  {logdir}/regularizers
